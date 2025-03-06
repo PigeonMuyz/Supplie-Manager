9dc3c46b891cf5e90bc98d7d948e828b565df9b2
Muyz <muyz0319@icloud.com>
2025-03-06 14:34:04 +0800

    Initial Commit

---
+++ Supplie Manager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Supplie Manager
+//
+//  Created by 黄天晨 on 2025/3/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Supplie Manager/Supplie_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Supplie_ManagerApp.swift
+//  Supplie Manager
+//
+//  Created by 黄天晨 on 2025/3/6.
+//
+
+import SwiftUI
+
+@main
+struct Supplie_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
